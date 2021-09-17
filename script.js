47e752e8404e6e598e38631f8c19b4e3b0ab6b8d
janneke-hub <86882640+janneke-hub@users.noreply.github.com>
2021-09-17 12:33:09 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let age = 15;
+
+if (age > 17) {
+    console.log("Je mag naar binnen.");
+} else {
+    console.log("Je bent te jong en wordt verzocht om te vertrekken.");
+}
+
+let isFemale = true;
+
+if (isFemale = false) {
+    console.log("Je bent welkom bij de ladiesnight");
+} else {
+    console.log("Vanavond is ladiesnight dus je wordt verzocht om te vertrekken");
+}
+
+let driverStatus = bob;
+
+if (bob = true) {
+    console.log("Je bent BOB dus je mag naar huis rijden.");
+} else {
+    console.log("Je bent geen BOB dus je mag niet naar huis rijden.");
+}
